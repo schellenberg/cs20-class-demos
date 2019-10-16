3adb4415f8b0970fdefc7fb1e1467292c853ef05
Dan Schellenberg <schellenberg@gmail.com>
2019-10-16 09:18:47 -0600

    Create 4-f-strings.py

---
+++ 4-f-strings.py
@@ -0,0 +1,9 @@
+name = input("What's your name? ")
+birthday = input("What's your birthday? ")
+age = input("How old are you turning? ")
+
+message = f"""Hello there, {name}!
+Your birthday is {birthday},
+and you will eventually turn {age}."""
+
+print(message)
