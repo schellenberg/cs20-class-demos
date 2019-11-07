8b2352837750083ae0f2f5263bc577ed29e167f0
Dan Schellenberg <schellenberg@gmail.com>
2019-11-07 14:31:38 -0600

    updated demos

--- 19-rectangle-shape.py
+++ 19-rectangle-shape.py
@@ -4,14 +4,17 @@ canvas = turtle.Screen()
 
 chris = turtle.Turtle()
 chris.pensize(4)
+#chris.speed(1)
 
 def draw_rectangle(some_turtle, length, width):
     for counter in range(2):
-        some_turtle.forward(length)
-        some_turtle.left(90)
         some_turtle.forward(width)
         some_turtle.left(90)
-    
-for rectangle in range(4):
+        some_turtle.forward(length)
+        some_turtle.left(90)
+        
+for rectangle in range(8):
     draw_rectangle(chris, 200, 75)
-    chris.left(90)
+    chris.forward(75)
+    chris.right(360/8)
+    
