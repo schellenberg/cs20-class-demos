d40bbbbc60eed7d56fdf14c15b7939e338775422
Dan Schellenberg <schellenberg@gmail.com>
2019-11-07 15:32:36 -0600

    Update 19-rectangle-shape-p5.py

--- 19-rectangle-shape-p5.py
+++ 19-rectangle-shape-p5.py
@@ -11,7 +11,10 @@ def draw_rectangle(some_turtle, side_length, side_width):
         some_turtle.left(90)
         some_turtle.forward(side_width)
         some_turtle.left(90)
+    some_turtle.left(90)
+    some_turtle.forward(side_width)
+    some_turtle.right(90)
 
-for rectangle in range(4):
+for rectangle in range(8):
     draw_rectangle(ray, 300, 75)
-    ray.left(90)
+    ray.left(360/8)
