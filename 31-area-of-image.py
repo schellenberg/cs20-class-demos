acafdf1433242df7683a455fd4f538dcb450e254
Dan Schellenberg <schellenberg@gmail.com>
2019-11-28 09:31:45 -0600

    Update 31-area-of-image.py

--- 31-area-of-image.py
+++ 31-area-of-image.py
@@ -1,28 +1,32 @@
 import image
 
-moon = image.Image("sneakers.jpeg")
+rooster = image.Image("rooster.jpeg")
+smile = image.Image("smile.png")
 
-width = moon.get_width()
-height = moon.get_height()
+width = rooster.get_width()
+height = rooster.get_height()
+
+smile_width = smile.get_width()
+smile_height = smile.get_height()
 
 canvas = image.ImageWin(width, height)
-moon.draw(canvas)
+rooster.draw(canvas)
 
-for y in range(height):
-    for x in range(width):
-        this_pixel = moon.get_pixel(x, y)
+for y in range(smile_height):
+    for x in range(smile_width):
+        this_pixel = smile.get_pixel(x, y)
         
         r = this_pixel.get_red()
         g = this_pixel.get_green()
         b = this_pixel.get_blue()
         
-        if x <= width/2:
-            average = (r + g + b) / 3
-            new_pixel = image.Pixel(average, average, average)
-        else:
-            new_pixel = image.Pixel(r, g, b)
+#         if x <= width/2:
+#             average = (r + g + b) / 3
+#             new_pixel = image.Pixel(average, average, average)
+#         else:
+#             new_pixel = image.Pixel(r, g, b)
         
-        moon.set_pixel(x, y, new_pixel)
+        rooster.set_pixel(x, y, this_pixel)
         
-    moon.draw(canvas)
+    rooster.draw(canvas)
         
