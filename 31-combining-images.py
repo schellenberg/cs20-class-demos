7fbc93e2264d9bd37b8696cc47345f8ad8764274
Dan Schellenberg <schellenberg@gmail.com>
2019-11-29 09:13:43 -0600

    Create 31-combining-images.py

---
+++ 31-combining-images.py
@@ -0,0 +1,27 @@
+import image
+
+rooster = image.Image("rooster.jpeg")
+smile = image.Image("smile.png")
+
+width = rooster.get_width()
+height = rooster.get_height()
+
+smile_width = smile.get_width()
+smile_height = smile.get_height()
+
+canvas = image.ImageWin(width, height)
+rooster.draw(canvas)
+
+for y in range(smile_height):
+    for x in range(smile_width):
+        this_pixel = smile.get_pixel(x, y)
+        
+        r = this_pixel.get_red()
+        g = this_pixel.get_green()
+        b = this_pixel.get_blue()
+        
+        if r < 250 and g < 250 and b < 250:
+            rooster.set_pixel(x + 150, y + 50, this_pixel)
+        
+    rooster.draw(canvas)
+        
