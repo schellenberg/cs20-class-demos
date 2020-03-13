855e8f073646dc38c6a00f95e6bcfc0704d9d51f
Dan Schellenberg <schellenberg@gmail.com>
2020-03-13 09:50:52 -0600

    first day demos

---
+++ 00-first-demo.py
@@ -0,0 +1,4 @@
+number = input("What's your fav number?")
+number = int(number)
+distance = 100 - number
+print("Your number is " + str(distance) + " away from 100")
---
+++ 01-module-demo.py
@@ -0,0 +1,8 @@
+# import math
+# print(math.cos(math.pi))
+
+# import webbrowser
+# webbrowser.open("http://wmcicompsci.ca")
+
+import random
+print(random.randrange(1, 101))
