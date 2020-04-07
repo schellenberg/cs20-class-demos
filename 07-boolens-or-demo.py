236243eaedcc7fc2a28be3fc35d1ed737827e319
Schellenberg <SchellenbergD@spsd.sk.ca>
2020-04-07 10:28:56 -0600

    demos done during zoom

---
+++ 07-boolens-or-demo.py
@@ -0,0 +1,8 @@
+name = input("Please enter your name: ")
+boredom_level = input("Please enter your boredom level: ")
+
+if (name == "allison" or name == "Allison") and boredom_level == 10:
+    print("Hey there!")
+
+else:
+    print("Go away")
