14dd853eb5f684b8c50398739b494b13702ebe1d
Schellenberg <SchellenbergD@spsd.sk.ca>
2020-05-05 10:26:06 -0600

    demos

---
+++ 09-ozayr-multiple-lists-demo.py
@@ -0,0 +1,9 @@
+instructions = [{"distance": 10, "angle": 15},
+                {"distance": 20, "angle": 30},
+                {"distance": 40, "angle": 50}]
+
+
+for task in instructions:
+    print(task["distance"])
+    print(task["angle"])
+    print()
