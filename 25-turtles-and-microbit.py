5cd3ee1ed0db0546e8167e381a85a833f2246318
Dan Schellenberg <schellenberg@gmail.com>
2020-10-02 15:09:51 -0600

    Update 25-turtles-and-microbit.py

--- 25-turtles-and-microbit.py
+++ 25-turtles-and-microbit.py
@@ -2,8 +2,20 @@ import microbit
 import time
 import turtle
 
+canvas = turtle.Screen()
+
+jack = turtle.Turtle()
+jack.color("blue")
+
 while True:
     x = microbit.accelerometer.get_x()
-    print(x)
     
-    time.sleep(0.5)
+    if x > 300:
+        print("RIGHT")
+        jack.forward(30)
+    
+    elif x < -300:
+        print("LEFT")
+        jack.backward(30)
+    
+    #time.sleep(0.5)
