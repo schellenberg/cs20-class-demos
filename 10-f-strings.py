dde5f6f0ef996326828f5410fe0627d3b988bcd4
schellenberg <227656+schellenberg@users.noreply.github.com>
2020-11-23 09:44:25 -0600

    string and gui demos

---
+++ 10-f-strings.py
@@ -0,0 +1,8 @@
+name = "Eli"
+age = 14
+
+greeting = f"Hello, {name}. I heard you just turned {age}!"
+print(greeting)
+
+# this code will print the following:
+# Hello, Eli. I heard you just turned 14!
