1cf3887297e01eb6492aec3040bcb89b5ae94a99
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2020-12-09 10:14:37 -0600

    Create 43-add-numbers.py

---
+++ 43-add-numbers.py
@@ -0,0 +1,9 @@
+def add_numbers(first_number, second_number):
+    the_sum = first_number + second_number
+    return the_sum
+
+def square_number(number):
+    the_square = number ** 2
+    return the_square
+
+print(square_number(3))
