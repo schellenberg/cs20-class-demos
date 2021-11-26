7d2498065895eb759221c3e629147d60e79189c1
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-11-26 09:19:50 -0600

    classroom demo

---
+++ 13-add-subtract-better.py
@@ -0,0 +1,27 @@
+# Add/Subtract Two Numbers
+# Dan Schellenberg
+# Nov 26, 2021
+
+# first ask the user whether they want to add or subtract
+operation = input("Do you want to add or subtract? ")
+
+if operation == "add" or operation == "subtract":
+    # take in the two numbers
+    first_number = input("What's the first number? ")
+    second_number = input("What's the second number? ")
+
+    # convert data types
+    first_number = float(first_number)
+    second_number = float(second_number)
+
+    # perform the required operation
+    if operation == "add":
+        answer = first_number + second_number
+    elif operation == "subtract":
+        answer = first_number - second_number
+        
+    # print the result
+    print("The answer is " + str(answer))
+
+else:
+    print("Error. That wasn't an option")
