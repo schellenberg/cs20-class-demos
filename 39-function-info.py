08c64a5ffcdb8de7a68899919d85fa81d0591093
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-12-09 09:42:14 -0600

    function demos

---
+++ 39-function-info.py
@@ -0,0 +1,5 @@
+def say_hi(name):
+    print("Hey there " + name)
+
+say_hi("Joel")
+print(name)
---
+++ 40-squared-function.py
@@ -0,0 +1,8 @@
+def square_number(number):
+    squared_value = number * number
+    return squared_value
+
+to_square = 3
+result = square_number(to_square)
+print(f"The square of {to_square} is {result}")
+# print(squared_value)
