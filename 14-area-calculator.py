9008e62e37f2f73d81c18064fbd9ce3fbc17b0ae
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-03-17 11:44:06 -0600

    demos

---
+++ 14-area-calculator.py
@@ -0,0 +1,15 @@
+# Area of a Circle Calculator
+# Dan Schellenberg
+# Mar 16, 2022
+
+# get the radius
+radius = input("What is the radius of the circle? ")
+
+# convert data types
+radius = int(radius)
+
+# calculate area
+area = 3.14 * radius ** 2
+
+# output area
+print(area)
---
+++ 15-not-operator.py
@@ -0,0 +1,7 @@
+password = input("What's the password? ")
+
+while password != "WMCI":
+    print("Nope. Try again.")
+    password = input("What's the password? ")
+
+print("Go Mauraders!")
