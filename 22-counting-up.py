36b968b87958338b8399489e044504eb71340776
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-03-24 11:14:16 -0600

    while loops

---
+++ 22-counting-up.py
@@ -0,0 +1,6 @@
+# Write a program that will print out the numbers 1 to 100.
+
+counter = 1
+while counter <= 100:
+    print(counter)
+    counter = counter + 1
---
+++ 23-counting-down.py
@@ -0,0 +1,8 @@
+#Write a program that counts down from 20 to 1, then prints Blastoff!.
+
+counter = 20
+while counter >= 1:
+    print(counter)
+    counter = counter - 1
+
+print("Blastoff!")
