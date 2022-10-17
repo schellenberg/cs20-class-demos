9dd6516f35a89b89ab44362cd0cefc4812db1654
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-10-17 10:55:59 -0600

    more day 1

---
+++ Period 2 Demos/05-for-loop.py
@@ -0,0 +1,6 @@
+# for counter in range(10):
+#     print(counter)
+
+groceries = ["apples", "carrots", "milk"]
+for item in groceries:
+    print("Don't forget to buy " + item)
---
+++ Period 2 Demos/06-functions.py
@@ -0,0 +1,5 @@
+def say_hello(name):
+    print("Hi there, " + name + "!")
+    
+say_hello("Faith")
+say_hello("Zoe-Ann")
