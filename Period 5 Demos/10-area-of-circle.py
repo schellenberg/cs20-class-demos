5f7ea834c888665b7d7e6907c0a9557fa5ca5a5c
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-10-18 15:16:24 -0600

    Create 10-area-of-circle.py

---
+++ Period 5 Demos/10-area-of-circle.py
@@ -0,0 +1,11 @@
+#get user input
+radius = input("What's the radius? ")
+
+#convert data types
+radius = float(radius)
+
+#do math
+area = 3.14 * radius * radius
+
+#print result
+print("The area of the circle is " + str(area))
