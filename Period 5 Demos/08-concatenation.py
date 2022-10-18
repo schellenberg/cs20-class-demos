659861ff17e8c7bea023b7ba9bc1ae904f21ba56
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-10-18 15:04:38 -0600

    day 2

---
+++ Period 5 Demos/08-concatenation.py
@@ -0,0 +1,5 @@
+first_name = input("What's your first name? ")
+last_name = input("What's your last name? ")
+fav_class = input("What's your fav class? ")
+
+print("Hello there, " + first_name + " " + last_name)
