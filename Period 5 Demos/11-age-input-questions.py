1ac45e5673956b723cbcea308ee5a9743032783f
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-10-19 14:47:19 -0600

    more demos

---
+++ Period 5 Demos/11-age-input-questions.py
@@ -0,0 +1,13 @@
+age = input("How old are you? ")
+
+age = int(age)
+
+if age != 16:
+    print("You probably aren't getting your lisence this year...")
+
+if age > 20:
+    print("I trust your driving.")
+
+if age > 25:
+    print("Car rental companies trust you now.")
+    
---
+++ Period 5 Demos/12-name-input-question.py
@@ -0,0 +1,9 @@
+# Hanson    /   Alexandra
+
+name = input("What is your name? ")
+
+if name == "Hanson":
+    print("Hey there! Good to see you!")
+
+elif name == "Alexandra":
+    print("Welcome back!")
