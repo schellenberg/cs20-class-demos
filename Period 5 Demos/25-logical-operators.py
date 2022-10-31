8dc1810b4055ad59c2613394c9fcd2d8f16d34d7
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-10-31 15:18:46 -0600

    logical operators demos

---
+++ Period 5 Demos/25-logical-operators.py
@@ -0,0 +1,7 @@
+number = input("What's the number? ")
+number = int(number)
+
+if number == 23 or number == 42:
+    print("I like that number")
+else:
+    print("Get a better number!")
---
+++ Period 5 Demos/26-logical-practice.py
@@ -0,0 +1,4 @@
+x = 20
+y = 25
+
+print(not x == 25 and y != 30)
