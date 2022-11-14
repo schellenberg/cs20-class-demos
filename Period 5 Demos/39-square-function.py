69ce0bf3c4392b350d743c0d6a2d516bcc06cc59
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-11-14 15:12:36 -0600

    demos

---
+++ Period 5 Demos/39-square-function.py
@@ -0,0 +1,12 @@
+
+def square(some_number):
+    the_square = some_number * some_number
+    return the_square
+    
+
+def times_three(a_number):
+    return a_number * 3
+
+
+answer = times_three(4)
+print(answer)
