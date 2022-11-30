cbd443501f47e6a7b1213de62b9648eef65b13a9
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-11-30 11:01:18 -0600

    demos

--- Period 2 Demos/43-empty-image.py
+++ Period 2 Demos/43-empty-image.py
@@ -6,9 +6,19 @@ height = 400
 canvas = image.ImageWin(width, height)
 img = image.EmptyImage(width, height)
 
-some_pixel = image.Pixel(255, 0, 0)
+                        #R  G  B
+some_pixel = image.Pixel(0, 0, 255)
 
-for x in range(width):
-    img.set_pixel(x, 300, some_pixel)
+#two lines intersecting
+# for y in range(height):
+#     img.set_pixel(50, y, some_pixel)
+# 
+# for x in range(width):
+#     img.set_pixel(x, 300, some_pixel)
+
+#changing every pixel in the image
+for y in range(height):
+    for x in range(width):
+        img.set_pixel(x, y, some_pixel)
 
 img.draw(canvas)
