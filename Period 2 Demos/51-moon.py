bbdf6a2e81ad6e602d9544fcfd593329a6450412
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-12-02 10:30:17 -0600

    image demos

---
+++ Period 2 Demos/51-moon.py
@@ -0,0 +1,22 @@
+import image
+
+moon = image.Image("moon.jpg")
+width = moon.get_width()
+height = moon.get_height()
+
+window = image.ImageWin(width, height)
+moon.draw(window)
+
+for x in range(width):
+    for y in range(height):
+        current_pixel = moon.get_pixel(x, y)
+        
+        r = current_pixel.get_red()
+        g = current_pixel.get_green()
+        b = current_pixel.get_blue()
+        
+        if r + g + b > 50:
+            new_pixel = image.Pixel(r, g, b+80)
+            moon.set_pixel(x, y, new_pixel)
+    
+    moon.draw(window)
