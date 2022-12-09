46fcd6902dcd761088624fd4126259118048bf74
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-12-09 10:27:25 -0600

    string demos

---
+++ Period 2 Demos/55-string-iterating.py
@@ -0,0 +1,12 @@
+#iterate by value
+
+# fruit = "pineapple"
+# for letter in fruit:
+#     print(letter)
+
+
+#iterate by location/index
+fruit = "pineapple"
+for counter in range(len(fruit)):
+    print(fruit[counter])
+
