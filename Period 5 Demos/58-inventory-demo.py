68359bc7015837dfc8d2b94711a1228754ee7db8
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2023-01-10 15:22:59 -0600

    Update 58-inventory-demo.py

--- Period 5 Demos/58-inventory-demo.py
+++ Period 5 Demos/58-inventory-demo.py
@@ -6,6 +6,7 @@ inventory.append("room 1 key")
 #player tries to go through door
 if "room 1 key" in inventory:
     print("Door is open.")
+    inventory.remove("room 1 key")
 else:
     print("You need a key. Go get it.")
 
