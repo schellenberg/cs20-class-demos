bcf3bc218f7a6618c3c63675d227506356bcf99e
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2023-03-20 13:47:31 -0600

    input output demos

---
+++ 11-age.py
@@ -0,0 +1,12 @@
+#get input
+age = input("How old are you? ")
+
+#convert data type
+age = int(age)
+
+#ask question
+if age >= 16:
+    print("You can get your drivers!")
+
+if age == 15:
+    print("You can get your learners!")
---
+++ 12-alabama.py
@@ -0,0 +1,8 @@
+winner = input("Who's gonna win March Madness? ")
+
+if winner != "Alabama":   # != means does NOT equal
+    print("You wish!")
+    
+else:
+    print("I agree! They look strong!")
+    
