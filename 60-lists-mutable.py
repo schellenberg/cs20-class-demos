f74dae701aaff64d7b10f3721850935acd9a25c6
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2023-06-05 13:35:12 -0600

    lists intro

---
+++ 60-lists-mutable.py
@@ -0,0 +1,5 @@
+groceries = [["ice", "popsicles"], "paper", "apples", ["juice", "pop"]]
+
+groceries[1] = "grapefruit"
+
+print(groceries)
---
+++ 61-strings-immutable.py
@@ -0,0 +1,4 @@
+greeting = "Hello world!"
+#my_hello = "J" + greeting[1:]
+greeting[0] = "J"
+print(greeting)
