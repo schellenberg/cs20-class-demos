98db365181a7d30c58b95cb2f08b605c79438478
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2023-10-12 14:34:25 -0600

    first day

---
+++ Period 5 Demos/01-if-elif-else.py
@@ -0,0 +1,10 @@
+some_number = 5
+
+if some_number > 10:
+    print("Yup, it's pretty big")
+
+elif some_number < -10:
+    print("It's pretty small")
+    
+else:
+    print("Not too big, not too small!")
