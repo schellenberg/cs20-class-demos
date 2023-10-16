712be3d5acb26ed28be262a9e3d5f76b34340a5b
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2023-10-16 09:22:22 -0600

    demos

---
+++ Period 1 Demos/11-not-equals.py
@@ -0,0 +1,12 @@
+name = "Dareen"
+tourney = "Mustang"
+
+if tourney == "Mustang":
+    print("Good luck!")
+
+if name != "Dareen":
+    print("You are not winning the hitting competition!")
+    
+else:
+    print("Gonna dominate the hitting comp!")
+    
