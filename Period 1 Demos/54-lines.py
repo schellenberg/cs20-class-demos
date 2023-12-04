e17be0f2876e4ee48259f218da3d27e100980123
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2023-12-04 09:58:29 -0600

    image demos

---
+++ Period 1 Demos/54-lines.py
@@ -0,0 +1,18 @@
+import image
+
+width = 400
+height = 300
+
+canvas = image.ImageWin(width, height)
+the_image = image.EmptyImage(width, height)
+
+                       # R   G  B
+the_pixel = image.Pixel(255, 0, 0)
+for x in range(width):
+    the_image.set_pixel(x, 200, the_pixel)
+
+other_pixel = image.Pixel(0, 0, 255)
+for y in range(height):
+    the_image.set_pixel(300, y, other_pixel)
+
+the_image.draw(canvas)
