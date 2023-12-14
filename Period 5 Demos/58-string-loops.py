0ed501ae12a4bd8590b333cfd9934449bafdd746
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2023-12-14 14:45:04 -0600

    string demos

---
+++ Period 5 Demos/58-string-loops.py
@@ -0,0 +1,12 @@
+#iterate - just care about the values
+# fruit = "peach"
+# for letter in fruit:
+#     print(letter)
+  
+#iterate - care about locations
+fruit = "peach"
+for index in range(len(fruit)):
+    print(fruit[index])
+
+    
+    
---
+++ Period 5 Demos/59-in-not-in.py
@@ -0,0 +1,6 @@
+school = "Walter Murray"
+# print("r" in school)
+
+# print("ray" in school)
+
+print("ray" not in school)
