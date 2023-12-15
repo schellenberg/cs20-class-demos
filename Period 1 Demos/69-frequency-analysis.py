aa0225ae66320d99b88909ed3e1317a8b90e9959
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2023-12-15 09:19:50 -0600

    Update 69-frequency-analysis.py

--- Period 1 Demos/69-frequency-analysis.py
+++ Period 1 Demos/69-frequency-analysis.py
@@ -1,12 +1,20 @@
-the_text = "Walter Murray is a good school"
+the_text = "Walter Murray is a good school."
 
 def count_letters(text, letter):
     '''Return the percentage that letter shows up in text.'''
-    times_seen = 0
-    for character in text:
-        if character == letter:
-            times_seen = times_seen + 1
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
     
-    print(f"{letter} shows up {times_seen} times")
+    total_letters = 0
+    times_seen = 0
+    for character in text.lower():  #turn everything to lowercase
+        if character in alphabet:
+            total_letters = total_letters + 1
+            if character == letter:
+                times_seen = times_seen + 1
+    
+    percentage = (times_seen/total_letters) * 100
+    percentage = round(percentage, 2)
+    
+    print(f"{letter} shows up {percentage}%")
 
 count_letters(the_text, "c")
