3b3d1ac87c84263a157d9c58f4fd9a86a595e84e
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2024-03-28 10:49:32 -0600

    Create 13-more-string-demos.py

---
+++ Period 3/13-more-string-demos.py
@@ -0,0 +1,16 @@
+# sentence = '''"Oh no!", she exclaimed. "Ben's bike is broken!"'''
+# print(sentence)
+# 
+# poem = '''The light of a candle
+# is tranferred to another candle -
+# spring twilight.'''
+# print(poem)
+
+age = input("How old are you? ")
+age = int(age)
+
+if age > 16:
+    print(f"Yes, if you are {age}, you're old enough to drive!")
+    
+else:
+    print(f"Nope. If you are {age}, you can't drive yet!")
