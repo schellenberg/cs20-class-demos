5bf4c1599e91913eedc1d4ad714f6d31a800e46f
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2024-04-08 12:11:17 -0600

    demos

---
+++ Period 3/15-count-up.py
@@ -0,0 +1,4 @@
+counter = 1
+while counter <= 100:
+    print(counter)
+    counter += 1   #counter = counter + 1
---
+++ Period 3/17-kinda-password.py
@@ -0,0 +1,6 @@
+guess = input("What's the password? ")
+while guess != "sask":
+    print("That's not it! Try again!")
+    guess = input("What's the password? ")
+
+print("What a nice place!")
