1d5cbd179642afc19500ca3607ccd4def8fc34c7
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2024-05-27 11:56:50 -0600

    more image manipulation

---
+++ Period 3/51-green-screen.py
@@ -0,0 +1,40 @@
+import image
+
+moon = image.Image("moon.jpg")
+width = moon.get_width()
+height = moon.get_height()
+
+window = image.ImageWin(width, height)
+moon.draw(window)
+
+for y in range(height):
+    for x in range(width):
+        the_pixel = moon.get_pixel(x, y)
+        
+        r = the_pixel.get_red()
+        g = the_pixel.get_green()
+        b = the_pixel.get_blue()
+        
+        #manipulate the pixel
+        if r + g + b > 30:
+            new_r = r
+            new_g = g
+            new_b = b + 50
+        
+        else:
+            new_r = r
+            new_g = g
+            new_b = b
+            
+        new_pixel = image.Pixel(new_r, new_g, new_b)
+        moon.set_pixel(x, y, new_pixel)
+        
+    moon.draw(window)
+        
+        
+        
+        
+        
+        
+        
+        
