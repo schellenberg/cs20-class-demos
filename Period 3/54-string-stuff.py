1dc2f949274bf9ee97e6675ace933999c2345220
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2024-05-29 13:10:08 -0600

    Create 54-string-stuff.py

---
+++ Period 3/54-string-stuff.py
@@ -0,0 +1,24 @@
+# first_name = 'Sumaiya'
+# last_name = 'Akter'
+# 
+# print(f"{first_name} {last_name}")
+
+# school = "Walter Murray Collegiate"
+# print(school[-5])
+# print(school[19])
+# print(school[7:13])
+# print(school[10:18])
+# print(school[5:])
+# print(school[5:-3])
+
+# subject = "Math"
+# the_length = len(subject)
+# print(subject[the_length - 1])
+# print(subject[1:the_length])
+
+
+school = "Walter Murray"
+for character in school:
+    print(character)
+
+
