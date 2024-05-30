2589638eafa2adefa38297d3ced623999785d953
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2024-05-30 10:57:23 -0600

    strings

---
+++ Period 3/55-string-traversal-styles.py
@@ -0,0 +1,12 @@
+# use this when you don't care where the letters are
+# name = "Emily"
+# for letter in name:
+#     print(letter)
+    
+
+# use this when you care where the letter is
+name = "Emily"
+for index in range(len(name)):
+    print(name[index])
+    
+    
