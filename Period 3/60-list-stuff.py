8a81c83ee64ae8a89de6ec7f0bb5f48a9cbd49c2
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2024-06-10 11:56:19 -0600

    Create 60-list-stuff.py

---
+++ Period 3/60-list-stuff.py
@@ -0,0 +1,52 @@
+# people = ["Marie", "Oleh", "Abdallah"]
+# 
+# people.append("Saumya")
+# people.append("Faith")
+# 
+# print(people)
+# 
+# last = people.pop()
+# print(last)
+# 
+# print(people)
+
+# for person in people:
+#     print(f"Hello {person}!")
+
+# for index in range(len(people)):
+#     print(f"Hello {people[index]}!")    
+
+# print(len(people))
+# print(people[-1])
+# print(people[2])
+# print(people[0][3])
+# print("Dinel" in people)
+# print("Oleh" in people)
+
+# print(people[1:])
+
+
+# places = [["Saskatoon", "Regina"], ["Edmonton", "Calgary"], ["Winnipeg", "Brandon"]]
+
+# print(places[1])
+# print(places[1][0][3])
+
+
+# stuff = [5, 7, 4, 8, 10]
+# stuff[2] = 20
+# print(stuff)
+
+
+# greeting = "Hello world!"
+# greeting[0] = "J"
+# print(greeting)
+
+
+
+sentence = "This is some random sentence."
+words = sentence.split()
+print(words)
+
+
+
+
