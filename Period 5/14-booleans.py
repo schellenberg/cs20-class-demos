7ff6d47483d5d31ac2521cd7d96572246850f880
schellenberg <227656+schellenberg@users.noreply.github.com>
2024-10-17 14:58:03 -0600

    boolean demos

---
+++ Period 5/14-booleans.py
@@ -0,0 +1,9 @@
+number = input("What number should I check? ")
+
+number = int(number)
+
+if number == 6:
+    print("They're the same!")
+
+else:
+    print("They are different!")
---
+++ Period 5/15-does-not-equal.py
@@ -0,0 +1,7 @@
+name = input("What's your name? ")
+
+if name != "Jawad":
+    print("Welcome here, " + name)
+    
+else:
+    print("Go away, " + name)
