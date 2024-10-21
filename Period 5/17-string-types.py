763109167a4810c7db4916504ff0d0ad095e24fa
schellenberg <227656+schellenberg@users.noreply.github.com>
2024-10-21 14:38:18 -0600

    strings

---
+++ Period 5/17-string-types.py
@@ -0,0 +1,10 @@
+name = 'Ziqin'
+name = "Ceberta"
+
+sentence = '''"Oh no" said Audrey's friend'''
+
+poem = '''Once upon a midnight dreary,
+while I pondered,
+weak and weary,'''
+
+print(poem)
---
+++ Period 5/18-f-strings.py
@@ -0,0 +1,6 @@
+name = "Owen"
+age = 16
+
+greeting = f"Hey {name}! I hear you are {age} years old!"
+
+print(greeting)
