4757099d50d582a294068707aa772033c59fc951
schellenberg <227656+schellenberg@users.noreply.github.com>
2024-10-31 10:29:06 -0600

    Create 27-boolean-operators.py

---
+++ Period 5/27-boolean-operators.py
@@ -0,0 +1,29 @@
+# number = 5
+# print(number > 0 and number < 10)
+
+# number = 35
+# print(number % 2 == 0 or number % 3 == 0)
+
+#this is a screw up. Avoid this!
+# number = 7
+# if number == 5 or 6:
+#     print("Yes")
+# else:
+#     print("No")
+
+#this is the way...
+# number = 7
+# if number == 5 or number == 6:
+#     print("Yes")
+# else:
+#     print("No")
+
+
+raining = True
+cold = False
+
+print(not raining or not cold and raining)
+
+
+
+    
