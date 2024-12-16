b9b13fe450616007248a887499aadf1039d74b0b
schellenberg <227656+schellenberg@users.noreply.github.com>
2024-12-16 11:40:17 -0600

    image manipulation

---
+++ Period 3/61-smiling-rooster.py
@@ -0,0 +1,31 @@
+import image
+
+rooster = image.Image("rooster.jpg")
+smile = image.Image("smile.png")
+
+rooster_width = rooster.get_width()
+rooster_height = rooster.get_height()
+
+smile_width = smile.get_width()
+smile_height = smile.get_height()
+
+canvas = image.ImageWin(rooster_width, rooster_height)
+rooster.draw(canvas)
+
+for y in range(smile_height):
+    for x in range(smile_width):
+        the_pixel = smile.get_pixel(x, y)
+        
+        r = the_pixel.get_red()
+        g = the_pixel.get_green()
+        b = the_pixel.get_blue()
+        
+        if r < 250 and g < 250 and b < 250:
+            #it's not a white background pixel
+            rooster.set_pixel(x + 170, y + 55, the_pixel)
+    rooster.draw(canvas)
+        
+        
+        
+        
+        
