42d8cb2895f404bcfbd533f153d6613b5724a567
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-01-15 14:31:28 -0600

    Create 70-list-methods.py

---
+++ Period 5/70-list-methods.py
@@ -0,0 +1,10 @@
+people = ['Calli', 'Luke', 'Tyler']
+people.append('Emmett')
+people.append('Jawad')
+
+person = people.pop()
+other = people.pop()
+another = people.pop()
+
+print(people)
+print(another)
