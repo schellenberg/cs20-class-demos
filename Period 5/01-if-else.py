1746e511db2583064671f535f17a154419d39edf
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-03-13 12:51:48 -0600

    demos

---
+++ Period 5/01-if-else.py
@@ -0,0 +1,5 @@
+some_number = 15.3
+if some_number > 5:
+    print("Yup. It's bigger than 5.")
+else:
+    print("Nope. It's not bigger than 5.")
---
+++ Period 5/02-user-input.py
@@ -0,0 +1,13 @@
+#get input from user
+first_number = input("What's the first number? ")
+second_number = input("What's the second number? ")
+
+#convert data types
+first_number = int(first_number)
+second_number = int(second_number)
+
+#calculate the answer
+the_sum = first_number + second_number
+
+#print the result
+print(the_sum)
