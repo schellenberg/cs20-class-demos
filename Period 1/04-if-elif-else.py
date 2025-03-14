618baa95ac027d2ad3ed4347f89127a04c22b03c
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-03-14 09:24:16 -0600

    demos

--- Period 1/04-if-elif-else.py
+++ Period 1/04-if-elif-else.py
@@ -5,3 +5,12 @@ temperature = int(temperature)
 #if the temp is less than -15, tell them to wear a parka
 if temperature < -15:
     print("Wear a parka!")
+
+elif temperature < 0:
+    print("Wear a sweater!")
+    
+elif temperature < 15:
+    print("Wear a t-shirt!")
+
+else:
+    print("Wear shorts!")
---
+++ Period 1/06-for-loop.py
@@ -0,0 +1,12 @@
+# count from 1 to 20
+for counter in range(2,7):
+    print(counter)
+    
+# people = ["Trey", "Bat", "Harry"]
+# for person in people:
+#     print(person)
+
+# numbers = [0, 1, 2, 3, 4, 5]
+# for the_number in numbers:
+#     print(the_number)
+    
