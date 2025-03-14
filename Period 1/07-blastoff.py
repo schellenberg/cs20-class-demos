5b5ad363723a5518a5abc65c240cafe9756d4c48
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-03-14 09:51:52 -0600

    demos

---
+++ Period 1/07-blastoff.py
@@ -0,0 +1,14 @@
+# print the numbers 10 down to 1, then print blastoff!
+
+#while loop version
+# counter = 10
+# while counter > 0:
+#     print(counter)
+#     counter = counter - 1
+# print("Blastoff!")    
+    
+
+#for loop version
+for counter in range(10, 0, -1):
+    print(counter)
+print("Blastoff!")
