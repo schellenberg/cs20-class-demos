d910ebdba896f7686db2858a05ead95f1383f3ef
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-03-17 15:05:04 -0600

    demos

---
+++ Period 5/11-variable-names.py
@@ -0,0 +1,11 @@
+favourite_class = input("What's your favourite class? ")
+
+if favourite_class == "math":
+    print("Rational choice")
+
+elif favourite_class == "english":
+    print("I like reading too")
+
+else:
+    print("Yeah, that's a good class!")
+    
