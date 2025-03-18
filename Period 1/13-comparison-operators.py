6d180611ff3970bba48bbac01a3532d09faaab40
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-03-18 09:35:31 -0600

    demos

---
+++ Period 1/13-comparison-operators.py
@@ -0,0 +1,11 @@
+# print(type(True))
+# print(type(5 == 6))
+
+#comparison operators
+number_one = 15
+number_two = 16
+
+print(not number_one == number_two)
+print(number_one != number_two)
+print(number_one > number_two)
+print(not number_one <= number_two)
