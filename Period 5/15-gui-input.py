15ccb4face86a3f9d358620aa72294282a844377
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-03-19 14:56:08 -0600

    Create 15-gui-input.py

---
+++ Period 5/15-gui-input.py
@@ -0,0 +1,8 @@
+import FreeSimpleGUI as sg
+
+first_name = sg.popup_get_text("What's your first name? ")
+last_name = sg.popup_get_text("What's your last name? ")
+
+greeting = "Hi there, " + first_name + " " + last_name
+
+sg.popup(greeting)
