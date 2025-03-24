e8890cc9bf3aaa962790ea8f2db52c275cbec6c6
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-03-24 14:38:44 -0600

    Create 18-while-loop-review.py

---
+++ Period 5/18-while-loop-review.py
@@ -0,0 +1,18 @@
+#while loop that counts from 1 to 10
+# number = 1
+# while number < 11:
+#     number = number + 1
+#     print(number)
+
+
+#another demo
+# counter = 0
+# while counter < 10:
+#     print(counter)
+    
+
+#infinite loop
+while True:
+    print("hello")
+
+
