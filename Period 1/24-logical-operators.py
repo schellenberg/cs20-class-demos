a90f3e4ebbaf41e911ed7b5974e5a3193b5bdf8b
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-03-28 09:42:08 -0600

    Create 24-logical-operators.py

---
+++ Period 1/24-logical-operators.py
@@ -0,0 +1,14 @@
+# number = 15
+# print(number > 0 and number < 10)
+
+# num = 25
+# print(num % 2 == 0 or num % 3 == 0)
+
+name = input("What's your name? ")
+# if name == "Harry" or "Nathan":  #whoops -- that's a screw up
+if name == "Harry" or name == "Nathan":
+    print("Hello!")
+else:
+    print("Go away!")
+    
+    
