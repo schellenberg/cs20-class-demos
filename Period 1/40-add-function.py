a4f91e8c815507916cd41ed6efe2c56227e287eb
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-04-10 09:53:40 -0600

    fruitful functions

---
+++ Period 1/40-add-function.py
@@ -0,0 +1,7 @@
+def add_them(number_one, number_two, number_three):
+    '''Add the three numbers and send back the sum.'''
+    the_sum = number_one + number_two + number_three
+    return the_sum
+
+answer = add_them(2, 3, 4)
+print(f"The sum is {answer}")
