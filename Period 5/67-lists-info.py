c06f62f3425c8570892f9a94f5d41ddc95a81a7c
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-06-04 14:48:04 -0600

    Create 67-lists-info.py

---
+++ Period 5/67-lists-info.py
@@ -0,0 +1,27 @@
+# people = [["Elena", "Cash"], ["Louise", "Vaidehi", "Ethan"]]
+# print(people[1])
+# print(people[0][1])
+# print(people[1][2])
+# print(people[1][0][-3])
+
+# if "Elena" in people[0]:
+#     print("Here!")
+# else:
+#     print("Not here!")
+
+# print(len(people))
+
+# print(people[1][1:])
+
+# fruit = ["apple", "banana", "orange"]
+# fruit[-2] = "pear"
+# print(fruit)
+
+greeting = "Hello world!"
+greeting[0] = "J"
+print(greeting)
+
+
+
+
+
