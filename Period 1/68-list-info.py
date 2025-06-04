ae8148c0bf8ca207a28b8ee62c6cb30f76bcfb73
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-06-04 09:41:42 -0600

    Create 68-list-info.py

---
+++ Period 1/68-list-info.py
@@ -0,0 +1,24 @@
+# people = [["Harry", "Bat", "Trey", "Liam"], ["Graham", "Abdullah", "Aayan", "Joel"]]
+# print(people[0][1])
+# print(len(people))
+# print(people[0][-2][-3])
+
+# if "Bat" in people[0]:
+#     print("He's here!")
+# else:
+#     print("Not here")
+
+# print(people[1][:2])
+# print(people[0][1:])
+
+# fruit = ["banana", "apple", "orange"]
+# fruit[-2] = "pear"
+# print(fruit)
+
+greeting = "Hello world!"
+greeting[0] = "J"
+print(greeting)
+
+# greeting = "Hello world!"
+# new_greeting = "J" + greeting[1:]
+# print(new_greeting)
