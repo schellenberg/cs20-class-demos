54d44c1f38246feff9658af9e857d8e6423430ca
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2025-10-08 13:38:09 -0600

    day 1

---
+++ Period 4/01-if-else.py
@@ -0,0 +1,5 @@
+some_number = 300
+if some_number > 100:
+    print("That's a big number!")
+else:
+    print("That's not so big.")
---
+++ Period 4/02-data-types.py
@@ -0,0 +1,16 @@
+# name = "Mitt"
+# print(type(name))
+
+# age = 17
+# print(type(age))
+
+# age = 17.25
+# print(type(age))
+
+# answer = True
+# print(type(answer))
+
+
+thing = 5.0
+thing = int(thing)
+print(thing)
