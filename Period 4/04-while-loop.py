f6d2d42590f0b8fc4f2841484bcb991076f47eed
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2025-10-08 14:16:47 -0600

    day 1

---
+++ Period 4/04-while-loop.py
@@ -0,0 +1,14 @@
+# counter = 1
+# while counter < 11:
+#     print(counter)
+#     counter = counter + 1
+
+# count down from 100 to 1
+counter = 100
+while counter > 0:
+    print(counter)
+    counter = counter - 1
+    
+    
+    
+    
---
+++ Period 4/06-functions.py
@@ -0,0 +1,5 @@
+def say_hello(the_name):
+    print("Hey there " + the_name)
+    
+say_hello("Milo")
+say_hello("Wahid")
