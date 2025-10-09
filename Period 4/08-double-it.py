03d6e8c240e0f6e0cc32dd28a56bb697f4be47ce
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2025-10-09 13:47:34 -0600

    day 2

---
+++ Period 4/08-double-it.py
@@ -0,0 +1,13 @@
+# double it program
+
+# get the info
+the_number = input("What's the number? ")
+
+#convert data types
+the_number = int(the_number)
+
+#do the math
+doubled = the_number * 2
+
+#print the answer
+print(the_number + " doubled is " + doubled)
---
+++ Period 4/09-keywords.py
@@ -0,0 +1,4 @@
+
+fav_class = input("What's your favourite class? ")
+
+print("Nice! I like " + fav_class + " too!")
