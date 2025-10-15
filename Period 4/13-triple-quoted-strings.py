a0ef8077ba7937c22f70e05e004a9354c5013f66
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2025-10-15 13:37:57 -0600

    strings

---
+++ Period 4/13-triple-quoted-strings.py
@@ -0,0 +1,19 @@
+# poem = """Once upon a midnight dreary,
+# while I pondered, weak and weary,"""
+# 
+# print(poem)
+
+
+# Hello James,
+# I hear you are 16 years old.
+# Do you still like Stranger Things?
+
+name = input("What's your name? ")
+age = input("How old are you? ")
+tv_show = input("What's your favourite TV show? ")
+
+the_message = f'''Hello {name},
+I hear you are {age} years old.
+Do you still like {tv_show}?'''
+
+print(the_message)
