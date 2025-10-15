11ab7788e6098523359e6d62183e86751c328e25
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2025-10-15 10:22:15 -0600

    strings

---
+++ Period 2/12-f-strings.py
@@ -0,0 +1,6 @@
+first_name = input("First name: ")
+last_name = input("Last name: ")
+age = 16
+
+message = f"Good to see you {first_name} {last_name}! I hear you are {age} years old."
+print(message)
---
+++ Period 2/13-triple-quoted-strings.py
@@ -0,0 +1,15 @@
+# poem = """Once upon a midnight dreary,
+# while I pondered, weak and weary"""
+# 
+# print(poem)
+
+
+name = "Kalman"
+age = 16
+tv_show = "Family Guy"
+
+the_message = f'''Hello {name}.
+I hear you turned {age} years old.
+Do you still enjoy {tv_show}?'''
+
+print(the_message)
