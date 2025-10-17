55686c7ada1b94805f9c83a0e0f52376f4f54c65
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2025-10-17 10:14:11 -0600

    Create 15-while-loop-debugging.py

---
+++ Period 2/15-while-loop-debugging.py
@@ -0,0 +1,15 @@
+# counter = 0
+# while counter < 5:
+#     counter = counter + 1
+#     print(counter)
+
+
+#flag controlled loop
+is_tim = False
+while is_tim == False:
+    name = input("Who are you? ")
+    if name == "Tim":
+        is_tim = True
+print("Oh. Hey Tim!")
+
+        
