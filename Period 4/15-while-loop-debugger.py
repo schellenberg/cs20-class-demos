e001dfffc722ae78358fa279802793463975bdc1
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2025-10-17 13:31:12 -0600

    Create 15-while-loop-debugger.py

---
+++ Period 4/15-while-loop-debugger.py
@@ -0,0 +1,14 @@
+# counter = 0
+# while counter < 5:
+#     counter = counter + 1
+#     print(counter)
+    
+
+#flag controlled loop
+still_looking = True
+while still_looking:
+    name = input("Who are you? ")
+    if name == "Mitt":
+        still_looking = False
+        
+print("Oh, hey Mitt!")        
