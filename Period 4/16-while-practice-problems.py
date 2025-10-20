ce61932382dcfefd35a2aad7bdb4eb1b6e5ebffd
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2025-10-20 13:24:32 -0600

    Create 16-while-practice-problems.py

---
+++ Period 4/16-while-practice-problems.py
@@ -0,0 +1,24 @@
+# Write a program that will print out the numbers 1 to 100.
+# counter = 1
+# while counter < 101:
+#     print(counter)
+#     counter = counter + 1
+    
+#Write a program that counts down from 20 to 1, then prints Blastoff!.
+# counter = 20
+# while counter > 0:
+#     print(counter)
+#     counter = counter - 1
+# print("Blastoff!")
+
+# Write a program that asks the user to enter a password. Keep asking for the password until they enter “sask”. Once they have successfully typed in “sask”, print out What a great place!.
+still_looking_for_password = True
+while still_looking_for_password:
+    password = input("What's the password? ")
+    if password == "sask":
+        still_looking_for_password = False
+print("What a great place!")        
+
+
+
+
