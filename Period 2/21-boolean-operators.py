f713d9764df90c2e9e7a68a6d624924bac1f1b2e
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2025-10-24 10:15:55 -0600

    Create 21-boolean-operators.py

---
+++ Period 2/21-boolean-operators.py
@@ -0,0 +1,9 @@
+# x = 8
+# print(x > 0 and x < 10)
+# print(x % 2 == 0 or x % 3 == 0)
+
+number = 6
+if number == 5 or number == 7:
+    print("I like that number")
+else:
+    print("Nope. Not that one!")
