627bd569db5049f41503dc21e94ae2d502dad80d
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2025-12-16 13:55:10 -0600

    strings

---
+++ Period 4/56-string-methods.py
@@ -0,0 +1,9 @@
+the_string = "hello world"
+# print(the_string.capitalize())
+# print(the_string.upper())
+# print(the_string.index("o"))
+# print(the_string.index("b"))
+# print(the_string.find("o"))
+# print(the_string.find("b"))
+print(the_string.split())
+# print(the_string.split("o"))
