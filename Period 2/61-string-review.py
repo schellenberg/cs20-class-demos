ec12bd56930f5a105b4e79295ce600ba7e391664
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-01-08 09:54:09 -0600

    Create 61-string-review.py

---
+++ Period 2/61-string-review.py
@@ -0,0 +1,13 @@
+stuff = [42, "thing", "bob", True]
+# print(stuff[2])
+# print(len(stuff))
+# print(stuff[-3])
+# print(42 in stuff)
+# print(stuff[1:3])
+
+people = [["Nathan", "Dhyan"], ["Ali", "Shy", "Fahim", "Asher"]]
+# print(people[1])
+# print(people[1][2])
+# print(people[1][2][3])
+# print("Ali" in people)
+print(people[1][2:])
