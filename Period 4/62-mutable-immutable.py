466fb5ea5a46eaa71eb5f2b86984eba4e7d90d35
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-01-09 13:27:20 -0600

    Create 62-mutable-immutable.py

---
+++ Period 4/62-mutable-immutable.py
@@ -0,0 +1,8 @@
+# tv_shows = ["breaking bad", "stranger things", "better call saul"]
+# tv_shows[1] = "bojack horseman"
+# print(tv_shows)
+
+greeting = "Hello world!"
+greeting[0] = "J"
+print(greeting)
+
