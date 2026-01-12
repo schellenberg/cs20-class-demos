9951e22925a8ce85a4d312fe489dc276dac4e7ae
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-01-12 13:31:39 -0600

    Create 63-list-methods.py

---
+++ Period 4/63-list-methods.py
@@ -0,0 +1,12 @@
+people = ["Sathini", "Khaliun"]
+people.append("Azeem")
+people.append("Milo")
+
+people.sort()
+
+this_one = people.pop()
+other_one = people.pop()
+
+print(people)
+print(this_one)
+print(other_one)
