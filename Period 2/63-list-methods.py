f0ec30235b7744884037d9f0a445617de83ca21e
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-01-12 10:20:59 -0600

    Create 63-list-methods.py

---
+++ Period 2/63-list-methods.py
@@ -0,0 +1,12 @@
+people = ["Shunhe", "Chase"]
+people.append("Nouh")
+people.append("Ryley")
+
+people.sort()
+
+that_guy = people.pop()
+other_guy = people.pop()
+
+print(people)
+print(that_guy)
+print(other_guy)
