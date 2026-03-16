dc6eb52207bef7b8fb7d12262f27c06e1eab5f75
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-03-16 09:54:04 -0600

    python intro

---
+++ Period 1/04-while-loops.py
@@ -0,0 +1,15 @@
+# counter = 10
+# 
+# while counter > 0:
+#     print(counter)
+#     counter = counter - 1
+
+
+#count from 20 up to 50
+counter = 20
+
+while counter < 51:
+    print(counter)
+    counter = counter + 1
+    
+    
---
+++ Period 1/05-for-loops.py
@@ -0,0 +1,21 @@
+# for counter in range(5):
+#     print(counter)
+    
+# for counter in [0, 1, 2, 3]:
+#     print(counter)
+
+# for counter in ["bob", "john", "sally"]:
+#     print(counter)
+
+
+# for counter in range(10, 16):
+#     print(counter)
+
+
+# for counter in range(10, 101, 5):
+#     print(counter)
+
+groceries = ["apples", "milk", "carrots", "bananas"]
+for item in groceries:
+    print("Don't forget to buy " + item)
+
