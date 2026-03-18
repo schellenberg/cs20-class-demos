23fbadc237f5edaeb4f9334ad45a2441f5caf3f2
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-03-18 09:24:14 -0600

    Create 10-comparison-operators.py

---
+++ Period 1/10-comparison-operators.py
@@ -0,0 +1,14 @@
+fav_number = input("What's your favourite number? ")
+
+
+if fav_number != "42":
+    print("That's a dumb number")
+
+
+# if not fav_number == "42":
+#     print("That's a dumb number")
+    
+
+# fav_number = int(fav_number)
+# if fav_number > 42 or fav_number < 42:
+#     print("That's a dumb number!")
