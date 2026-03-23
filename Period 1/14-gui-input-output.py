d00107a287f1e2ac4c28e09af8e9f24c0844f3d7
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-03-23 11:43:27 -0600

    gui

---
+++ Period 1/14-gui-input-output.py
@@ -0,0 +1,7 @@
+import FreeSimpleGUI as sg
+
+first_name = sg.popup_get_text("What's your first name?")
+last_name = sg.popup_get_text("What's your last name?")
+
+message = f"Hello there {first_name} {last_name}!"
+sg.popup(message)
