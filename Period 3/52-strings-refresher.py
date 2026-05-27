917386778caf18264fa12b81849fbc7fef74d0d5
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-05-27 11:43:34 -0600

    Create 52-strings-refresher.py

---
+++ Period 3/52-strings-refresher.py
@@ -0,0 +1,36 @@
+city = "San Antonio"
+team = "Spurs"
+
+#concatenation
+# print(city + " " + team)
+
+#f-string
+# print(f"{city} {team}")
+
+#index values
+# print(team[-2])
+# print(city[4])
+
+#length
+# print(len(team))
+# print(len(city))
+# print(team[len(team)])
+
+
+city = "San Antonio"
+team = "Spurs"
+
+#slices
+# print(team[1:4])
+# print(city[4:7])
+# print(city[6:-1])
+# print(city[4:])
+# print(city[:3])
+# print(city[4:230489])
+
+#traversal
+# for counter in range(5):
+#     print(counter)
+    
+for letter in "Murray":
+    print(letter)
