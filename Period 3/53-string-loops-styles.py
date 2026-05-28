c5368645822db4a02043602068d7283a2acba875
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-05-28 11:44:25 -0600

    strings

---
+++ Period 3/53-string-loops-styles.py
@@ -0,0 +1,10 @@
+school = "Walter Murray"
+
+#if you don't care about where the letter is
+# for letter in school:
+#     print(letter)
+
+#if you DO care about where the letter is
+for index in range(len(school)):
+    letter = school[index]
+    print(letter)
---
+++ Period 3/54-string-inclusion.py
@@ -0,0 +1,4 @@
+school = "Walter Murray"
+# print("h" in school)
+# print("a" in school)
+print("tr" in school)
