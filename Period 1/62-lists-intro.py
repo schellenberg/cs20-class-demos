e7c355fe1bff9e12198740cb0957cf465ba12679
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-06-05 09:20:26 -0600

    Create 62-lists-intro.py

---
+++ Period 1/62-lists-intro.py
@@ -0,0 +1,33 @@
+# people = ["Andrei", "Tyrone", "Tyrone", "Hridhay", "Leo"]
+# print(people[-2][3])
+# print(len(people))
+
+# people = [["Amrita", "Rennata", "Sensi"], ["Yakup", "Zeca", "Eric"]]
+# print(people[0][1])
+# print(people[0][1][5])
+# print(len(people))
+
+# a_list = [3, 67, "cat", [56, 57, "dog"], [ ], 3.14, False]
+# print("cat" in a_list)
+# print("dog" in a_list)
+# print(a_list[2:5])
+
+# people = ["Andrei", "Tyrone", "Tyrone", "Hridhay", "Leo"]
+# people[1] = "Odin"
+# print(people)
+
+# greeting = "Hello world!"
+# greeting[0] = "J"
+# print(greeting)
+
+#easy for loop
+# people = ["Andrei", "Tyrone", "Tyrone", "Hridhay", "Leo"]
+# for person in people:
+#     print(person)
+
+#harder for loop -- care about index values
+people = ["Andrei", "Tyrone", "Tyrone", "Hridhay", "Leo"]
+for index in range(len(people)):
+    print(people[index])
+    
+    
