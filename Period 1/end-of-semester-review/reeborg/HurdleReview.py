78f2ff386026cbe995bd26a1f10737997ab2b634
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-06-10 09:34:24 -0600

    added end of semester review solutions

---
+++ Period 1/end-of-semester-review/reeborg/HurdleReview.py
@@ -0,0 +1,51 @@
+#move fast
+think(0)
+
+def turn_right():
+    repeat 3:
+        turn_left()
+
+def jump_hurdle():
+    #face north
+    turn_left()
+    #go all the way up the hurdle
+    while wall_on_right():
+        move()
+    #face east
+    turn_right()
+    #take one step 
+    move()
+    #if the hurdle extends horizontally, go to end of it
+    while wall_on_right():
+        move()
+    #face south
+    turn_right()
+    #head to south edge
+    while front_is_clear():
+        move()
+    #face east again, like i was at the start
+    turn_left()
+
+#keep going until you get to the flag/goal
+while not at_goal():
+    #if nothing in front of you, just move
+    if front_is_clear():
+        move()
+    #otherwise, jump over the hurdle
+    else:
+        jump_hurdle()
+        
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
+def thing():
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    move()
